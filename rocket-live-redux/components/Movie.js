cb2d161cb068cd826f35dcf78a1668c9c3d3cd4c
Gabriel Cesar <gcal1@live.com>
2017-10-12 21:38:39 -0300

    acessando api - api fora do ar

---
+++ rocket-live-redux/components/Movie.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { 
+    View, Text, StyleSheet, Image, TouchableOpacity, 
+} from 'react-native';
+
+export default class Movie extends React.Component {
+    render () {
+        return (
+            <View style={styles.container}>
+                <Image style={styles.poster} 
+                    resizeMode="contain" 
+                    source={{ 
+                        uri: this.props.movie.poster, 
+                        width: 276, 
+                        height: 395, 
+                    }} />
+                
+                <View style={styles.infoContainer}>
+                    <Text style={styles.title} >{ this.props.movie.show_title }</Text>
+                    
+                    <Text 
+                        style={styles.description} 
+                        numberOfLines={5} 
+                        ellipsizeMode="tail">
+                        { this.props.movie.summary }
+                    </Text>
+
+                    <TouchableOpacity   
+                        style={styles.button} 
+                        onPress={() => {}} >
+                        <Text style={styles.buttonText} >Adicionar aos favoritos</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        marginTop: 20,
+        paddingTop: 20,
+        borderTopWidth: 1,
+        borderColor: '#444',
+    },
+
+    poster: {
+        width: 120,
+        height: 172,
+    },
+
+    infoContainer: {
+        marginLeft: 10,
+        width: 200,
+    },
+
+    title: {
+        color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+
+    description: {
+        color: '#CCC',
+        marginTop: 10,
+        fontSize: 14,
+        lineHeight: 20,
+    }, 
+
+    button: {
+        backgroundColor: '#BE3232',
+        alignSelf: 'stretch',
+        height: 30,
+        borderRadius: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 10,
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontSize: 12, 
+        fontWeight: 'bold',
+    },
+});
--- rocket-live-redux/pages/Search.js
+++ rocket-live-redux/pages/Search.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { StyleSheet, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput, ActivityIndicator } from 'react-native';
+
+import Movie from '../components/Movie';
 
 export default class Search extends React.Component {
+    state = {
+        search: '',
+        movie: null,
+        loading: false,
+    };
+
+    fetchMovies = async (search) => {
+        if (search.length > 0) {
+            //request
+            this.setState({ loading: true });
+            try {
+                const response = await
+                    fetch(`http://netflixroulette.net/api/api.php?title=${search}`);
+                
+                if (!response.ok) throw {};
+
+                const movie = await response.json();
+                
+                this.setState({ movie, loading: false });
+            } catch (error) {
+                this.setState({ movie: null, loading: false });
+            }
+        } else {
+            this.setState({ movie: null });
+        }
+    };
+
     render () {
         return (
-            <View style={styles.containera}>
-                <TextInput style={styles.input} placeholder="Digite sua busca" />
+            <View style={styles.container}>
+                <TextInput 
+                    style={styles.input} 
+                    placeholder="Digite sua busca"
+                    onChange={this.fetchMovies} />
+
+                { this.state.loading &&
+                    <ActivityIndicator style={styles.loading} color="#FFF" size="small" />
+                } 
+
+                { this.state.movie && 
+                    <Movie movie={this.state.movie} />
+                }
+
             </View>
         );
     }
@@ -24,7 +65,7 @@ const styles = StyleSheet.create({
         height: 40,
         borderRadius: 3,
         marginTop: 20,
-        fontSize: 12,
+        fontSize: 16,
         paddingHorizontal: 20,
         //fontFamily: 'Arial',
     },
